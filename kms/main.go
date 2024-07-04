3a053ee1f29cce65deef4987746d17df190ff65f
KentaTakahashi <knwoop@gmail.com>
2024-07-04 09:43:09 +0900

    add create a key ring and create a key sample

--- kms/main.go
+++ kms/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	kms "cloud.google.com/go/kms/apiv1"
 	"cloud.google.com/go/kms/apiv1/kmspb"
@@ -19,6 +20,8 @@ func main() {
 
 	// Location in which to list key rings.
 	locationID := "global"
+	keyRingID := "jwt-key-ring"
+	keyID := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
 
 	// Create the client.
 	ctx := context.Background()
@@ -27,6 +30,41 @@ func main() {
 		log.Fatalf("failed to setup client: %v", err)
 	}
 	defer client.Close()
+
+	keyRingName := fmt.Sprintf("projects/%s/locations/%s/keyRings/%s", e.GoogleCloudProjectID, locationID, keyRingID)
+
+	// Create a key ring
+	_, err = client.CreateKeyRing(ctx, &kmspb.CreateKeyRingRequest{
+		Parent:    fmt.Sprintf("projects/%s/locations/%s", e.GoogleCloudProjectID, locationID),
+		KeyRingId: keyRingID,
+	})
+	if err != nil {
+		log.Fatalf("failed to create key ring: %v", err)
+	}
+
+	purpose := kmspb.CryptoKey_ASYMMETRIC_SIGN
+	algorithm := kmspb.CryptoKeyVersion_EC_SIGN_P256_SHA256
+	cryptoKey := &kmspb.CryptoKey{
+		Purpose: purpose,
+		VersionTemplate: &kmspb.CryptoKeyVersionTemplate{
+			Algorithm: algorithm,
+		},
+	}
+
+	// 鍵のフルパス
+	cryptoKeyName := fmt.Sprintf("%s/cryptoKeys/%s", keyRingName, keyID)
+
+	// 鍵を作成
+	_, err = client.CreateCryptoKey(ctx, &kmspb.CreateCryptoKeyRequest{
+		Parent:      keyRingName,
+		CryptoKeyId: keyID,
+		CryptoKey:   cryptoKey,
+	})
+	if err != nil {
+		log.Fatalf("failed to create crypto key: %v", err)
+	}
+
+	fmt.Println("Crypto key created:", cryptoKeyName)
 
 	// Create the request to list KeyRings.
 	listKeyRingsReq := &kmspb.ListKeyRingsRequest{
